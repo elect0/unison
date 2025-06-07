66a17dae41f1660c149f68c31abd4bf8c0a93180
elect0 <educinca8@gmail.com>
2025-06-07 10:15:51 -0700

    feat: implement user registration and login endpoints

--- backend/src/http/routes/auth.routes.ts
+++ backend/src/http/routes/auth.routes.ts
@@ -2,7 +2,7 @@ import { Elysia, t } from "elysia";
 import { lucia } from "../../services/auth.service";
 import { users } from "../../db/schema";
 import { Argon2id } from "oslo/password";
-import { generateRandomString, alphabet } from "oslo/crypto";
+import { generateRandomString, alphabet, generate } from "oslo/crypto";
 import { db } from "../../db";
 
 export const authRoutes = new Elysia({ prefix: "/auth" })
@@ -11,13 +11,6 @@ export const authRoutes = new Elysia({ prefix: "/auth" })
     async ({ body, set }) => {
       const { username, email, password } = body;
 
-      const existingUser = await db.query.users.findFirst({
-        where: (users, { eq }) => eq(users.email, email),
-      });
-      if (existingUser) {
-        set.status = 400;
-        return { error: "User already exists." };
-      }
       const argon2id = new Argon2id();
       const hashedPassword = await argon2id.hash(password);
       const userId = generateRandomString(16, alphabet("a-z", "0-9"));
@@ -44,9 +37,9 @@ export const authRoutes = new Elysia({ prefix: "/auth" })
     },
     {
       body: t.Object({
-        username: t.String(),
-        email: t.String(),
-        password: t.String(),
+        username: t.String({ minLength: 2 }), // Match the schema and require at least 2 characters
+        email: t.String({ format: "email" }), // Ensures it's a valid email format
+        password: t.String({ minLength: 8 }), // Enforce a minimum password length
       }),
     }
   )
@@ -61,13 +54,13 @@ export const authRoutes = new Elysia({ prefix: "/auth" })
         if (!user) {
           set.status = 404;
 
-          return { error: "Couldn't find any user." };
+          return { error: "Incorrect email or password" };
         }
         const argon2id = new Argon2id();
         const isValidPassword = await argon2id.verify(user.password, password);
         if (!isValidPassword) {
           set.status = 401;
-          return { error: "Invalid password." };
+          return { error: "Incorrect email or password" };
         }
         const session = await lucia.createSession(user.id, {});
         const sessionCookie = lucia.createSessionCookie(session.id);
@@ -79,7 +72,7 @@ export const authRoutes = new Elysia({ prefix: "/auth" })
     },
     {
       body: t.Object({
-        email: t.String(),
+        email: t.String({ format: "email" }),
         password: t.String(),
       }),
     }
